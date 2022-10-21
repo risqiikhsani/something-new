a25d24dd90a4f2bd53a306fb7db3a5c5f6728c68
risqi2 <risqiikhsani12@gmail.com>
2022-10-21 14:23:31 +0700

    test

--- user/backends.py
+++ user/backends.py
@@ -1,3 +1,5 @@
+from ast import Or
+from asyncio.windows_events import NULL
 from django.contrib.auth.backends import ModelBackend
 
 from django.db.models import Q
@@ -5,7 +7,7 @@ from django.db.models import Q
 
 from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth.hashers import check_password
-
+from django.db.models import Q
 from django.contrib.auth import get_user_model
 CustomUser = get_user_model()
 
@@ -19,11 +21,18 @@ CustomUser = get_user_model()
 
 class AuthBackend(ModelBackend):
     def authenticate(self, request, **kwargs):
+        print("custom backend is running")
         username = kwargs['username']
         password = kwargs['password']
         try:
-            user = CustomUser.objects.get(username=username)
+            print("check username is running")
+            # user = CustomUser.objects.get(username=username)
+            user = CustomUser.objects.get(
+                Q(username__exact=username) | 
+                Q(email__exact=username) |
+                Q(phone_number__exact=username)
+            )
             if user.check_password(password) is True:
                 return user
-        except user.DoesNotExist:
+        except CustomUser.DoesNotExist:
             pass
