db58d419a61a327589f7b1d6a492b89b59298414
risqi2 <risqiikhsani12@gmail.com>
2022-11-09 19:58:13 +0700

    test

--- app/serializers.py
+++ app/serializers.py
@@ -9,80 +9,72 @@ from versatileimagefield.serializers import VersatileImageFieldSerializer
 from user.serializers import User_Serializer
 
 
-class PostList_Serializer(serializers.ModelSerializer):
+class Post_Serializer(serializers.ModelSerializer):
     user = User_Serializer(required=False)
     likes_amount = serializers.IntegerField(source="get_likes_amount")
     comments_amount = serializers.IntegerField(source="get_comments_amount")
-    # shares_amount =
-    # liked = serializers.SerializerMethodField()
-    # shared =
-    # saved =
+    shares_amount = serializers.IntegerField(source="get_shares_amount")
+    liked = serializers.SerializerMethodField()
+    shared = serializers.SerializerMethodField()
+    saved = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = ['id', 'user', 'text', 'time_creation',
-            'likes_amount', 'comments_amount']
+            'likes_amount', 'comments_amount','shares_amount','liked','shared','saved']
         read_only_fields = ['user']
 
 	# def get_liked(self,obj):
 	# 	if obj.like_set
+    def get_liked(self,obj):
+        if obj.like_set.all().get(user=self.context['request'].user).exists():
+            return True
+        else:
+            return False
+
+    def get_shared(self,obj):
+        if obj.share_set.all().get(user=self.context['request'].user).exists():
+            return True
+        else:
+            return False
+
+    def get_saved(self,obj):
+        if obj.save_set.all().get(user=self.context['request'].user).exists():
+            return True
+        else:
+            return False
 
 
-class PostDetail_Serializer(serializers.ModelSerializer):
+
+class Comment_Serializer(serializers.ModelSerializer):
     user = User_Serializer(required=False)
     likes_amount = serializers.IntegerField(source="get_likes_amount")
-    comments_amount = serializers.IntegerField(source="get_comments_amount")
-    # likes_amount = serializers.
-    # comments_amount =
-    # shares_amount =
-    # liked =
-    # shared =
-    # saved =
-    class Meta:
-        model = Post
-        fields = ['id','user','text','time_creation','likes_amount','comments_amount']
-        read_only_fields = ['user']
-
-
-class CommentList_Serializer(serializers.ModelSerializer):
-    user = User_Serializer(required=False)
-    likes_amount = serializers.IntegerField(source="get_likes_amount")
-    # likes_amount =
-    # liked =
-    class Meta:
-        model = Comment
-        fields = ['post','user','text','time_creation','likes_amount']
-        read_only_fields = ['user', 'post']
-
-
-class CommentDetail_Serializer(serializers.ModelSerializer):
-    user = User_Serializer(required=False)
-    likes_amount = serializers.IntegerField(source="get_likes_amount")
-    # likes_amount =
-    # liked =
+    replies_amount = serializers.IntegerField(source="get_replies_amount")
+    liked = serializers.SerializerMethodField()
     class Meta:
         model = Comment
-        fields = ['post','user','text','time_creation','likes_amount']
+        fields = ['post','user','text','time_creation','likes_amount','replies_amount','liked']
         read_only_fields = ['user', 'post']
 
+    def get_liked(self,obj):
+        if obj.like_set.all().get(user=self.context['request'].user).exists():
+            return True
+        else:
+            return False
 
-class ReplyList_Serializer(serializers.ModelSerializer):
+
+
+class Reply_Serializer(serializers.ModelSerializer):
     user = User_Serializer(required=False)
     likes_amount = serializers.IntegerField(source="get_likes_amount")
-    # likes_amount =
-    # liked =
+    liked = serializers.SerializerMethodField()
     class Meta:
         model = Reply
-        fields = ['comment','user','text','time_creation','likes_amount']
+        fields = ['comment','user','text','time_creation','likes_amount','liked']
         read_only_fields = ['user', 'comment']
 
-
-class ReplyDetail_Serializer(serializers.ModelSerializer):
-    user = User_Serializer(required=False)
-    likes_amount = serializers.IntegerField(source="get_likes_amount")
-    # likes_amount =
-    # liked =
-    class Meta:
-        model = Reply
-        fields = ['comment','user','text','time_creation','likes_amount']
-        read_only_fields = ['user', 'comment']
+    def get_liked(self,obj):
+        if obj.like_set.all().get(user=self.context['request'].user).exists():
+            return True
+        else:
+            return False
