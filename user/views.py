2dd7e66808a74713792d0a53c20bc7e4d284cc8f
risqi2 <risqiikhsani12@gmail.com>
2022-11-12 13:27:37 +0700

    test

--- user/views.py
+++ user/views.py
@@ -1,6 +1,9 @@
 
 
-
+from .serializers import User_Serializer
+from .serializers import Request_Serializer
+from .serializers import Connection_Serializer
+from django_filters.rest_framework import DjangoFilterBackend
 from http import server
 from os import stat
 
@@ -8,7 +11,7 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.db.models import Q
 from django.core.exceptions import ValidationError
 
-from rest_framework import generics, mixins,response, decorators, permissions, status
+from rest_framework import generics, mixins, response, decorators, permissions, status
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser, MultiPartParser, FormParser
 from rest_framework.decorators import api_view
@@ -27,33 +30,31 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-from django_filters.rest_framework import DjangoFilterBackend
-
 @api_view(['GET'])
 def api_root(request, format=None):
 	return Response({
 		'users': reverse('user-list', request=request, format=format),
-		
+
 	})
 
 # Using mixins
 # https://www.django-rest-framework.org/tutorial/3-class-based-views/
 
-from .serializers import User_Serializer
-class UserList(mixins.ListModelMixin,generics.GenericAPIView):
+
+class UserList(mixins.ListModelMixin, generics.GenericAPIView):
 	serializer_class = User_Serializer
 	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 	queryset = User.objects.all()
 
 	def get(self, request, *args, **kwargs):
-		return self.list(request,*args,**kwargs)
+		return self.list(request, *args, **kwargs)
 
-from .serializers import User_Serializer
+
 class UserDetail(mixins.RetrieveModelMixin,
 					mixins.UpdateModelMixin,
 					mixins.DestroyModelMixin,
 					generics.GenericAPIView):
-	queryset = User.objects.all()				
+	queryset = User.objects.all()
 	serializer_class = User_Serializer
 	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
@@ -61,24 +62,61 @@ class UserDetail(mixins.RetrieveModelMixin,
 		return self.retrieve(request, *args, **kwargs)
 
 
-
-from .serializers import Connection_Serializer
-class ConnectionList(mixins.ListModelMixin,generics.GenericAPIView):
+class ConnectionList(mixins.ListModelMixin, generics.GenericAPIView):
 	serializer_class = Connection_Serializer
 	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
 	def get_queryset(self):
-		return Connection.objects.all().get(user=self.request.user)
+		connection = Connection.objects.get(user=self.request.user)
+		return connection.connected.all()
 
 	def get(self, request, *args, **kwargs):
-		return self.list(request,*args,**kwargs)
+		return self.list(request, *args, **kwargs)
 
-from .serializers import Request_Serializer
-class RequestList(mixins.ListModelMixin,generics.GenericAPIView):
+
+class ConnetionDetail(generics.GenericAPIView):
+	serializer_class = Connection_Serializer
+	permission_classes = [
+	    permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+	def get_queryset(self):
+		return get_object_or_404(Connection, user=self.request.user)
+
+	def get(self, request, *args, **kwargs):
+		queryset = self.get_queryset()
+		connected = get_object_or_404(
+		    queryset.connected.all(), id=self.kwargs['connection_id'])
+		serializer = self.get_serializer(instance=connected)
+		return Response(serializer.data)
+
+	def delete(self, request, *args, **kwargs):
+		queryset = self.get_queryset()
+		connected = get_object_or_404(
+		    queryset.connected.all(), id=self.kwargs['connection_id'])
+		queryset.remove(connected)
+		return Response("deleted", status=status.HTTP_204_NO_CONTENT)
+
+
+class RequestList(mixins.ListModelMixin, generics.GenericAPIView):
 	serializer_class = Request_Serializer
 	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
 	def get_queryset(self):
 		return Request.objects.all().filter(user=self.request.user)
 
 	def get(self, request, *args, **kwargs):
-		return self.list(request,*args,**kwargs)
+		return self.list(request, *args, **kwargs)
 
+
+class RequestDetail(mixins.RetrieveModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+	serializer_class = Request_Serializer
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+	queryset = Request.objects.all()
+
+
+
+	def get(self, request, *args, **kwargs):
+		return self.retrieve(request, *args, **kwargs)
+	
+	def delete(self, request, *args, **kwargs):
+		return self.destroy(request, *args, **kwargs)
