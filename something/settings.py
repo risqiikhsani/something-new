550925f1c9b0ef7b92aaa44a8905452cf3a7a735
risqi2 <risqiikhsani12@gmail.com>
2022-11-17 21:08:40 +0700

    test

--- something/settings.py
+++ something/settings.py
@@ -169,11 +169,11 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": config("DB_NAME"),
-            "USER": config("DB_USER"),
-            "PASSWORD": config("DB_PWD"),
-            "HOST": config("DB_HOST"),
-            "PORT": config("DB_PORT"),
+            # "NAME": config("DB_NAME"),
+            # "USER": config("DB_USER"),
+            # "PASSWORD": config("DB_PWD"),
+            # "HOST": config("DB_HOST"),
+            # "PORT": config("DB_PORT"),
         }
     }
 
