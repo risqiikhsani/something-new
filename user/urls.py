734cf2ce78c619c7d582ca3e558c6659c98d9178
risqi <risqiikhsani16@gmail.com>
2023-01-01 17:27:33 +0700

    test

--- user/urls.py
+++ user/urls.py
@@ -27,30 +27,30 @@ from .views import (
 
 
 urlpatterns = [
-    path('register/',Register.as_view(), name='register'),
-    path('login/',Login.as_view(), name='login'),
-    path('token-refresh/', TokenRefreshView.as_view(), name='refresh-token'),
-    path('change-password/', ChangePassword.as_view(), name='change-password'),
+    path('register',Register.as_view(), name='register'),
+    path('login',Login.as_view(), name='login'),
+    path('token-refresh', TokenRefreshView.as_view(), name='refresh-token'),
+    path('change-password', ChangePassword.as_view(), name='change-password'),
     path('', api_root),
-    path('user-list/',UserList.as_view(), name='user-list'),
-    path('user-detail/',UserDetail.as_view(), name="user-detail"),
+    path('user-list',UserList.as_view(), name='user-list'),
+    path('user-detail',UserDetail.as_view(), name="user-detail"),
 
     # WORKS
-    path('connections/',ConnectionViewSet.as_view({'get':'list'}), name='connection-list'),
+    path('connections',ConnectionViewSet.as_view({'get':'list'}), name='connection-list'),
     # WORKS
-    path('user/<int:pk>/connections/',UserViewSet.as_view({'get':'user_connection_list'}), name='user-connection-list'),
+    path('user/<int:pk>/connections',UserViewSet.as_view({'get':'user_connection_list'}), name='user-connection-list'),
     # WORKS
-    path('connections/<int:pk>/remove/',ConnectionViewSet.as_view({'get':'remove_connection'}), name='connection-detail-remove'),
+    path('connections/<int:pk>/remove',ConnectionViewSet.as_view({'get':'remove_connection'}), name='connection-detail-remove'),
 
     # WORKS
-    path('requests/',RequestViewSet.as_view({'get':'list'}), name='request-list'),
+    path('requests',RequestViewSet.as_view({'get':'list'}), name='request-list'),
     # WORKS
-    path('user/<int:pk>/send_request/',UserViewSet.as_view({'get':'send_request'}),name='send-request'),
+    path('user/<int:pk>/send_request',UserViewSet.as_view({'get':'send_request'}),name='send-request'),
     # WORKS
-    path('user/<int:pk>/cancel_sent_request/',UserViewSet.as_view({'get':'cancel_sent_request'}),name='cancel-sent-request'),
+    path('user/<int:pk>/cancel_sent_request',UserViewSet.as_view({'get':'cancel_sent_request'}),name='cancel-sent-request'),
     # WORKS
-    path('requests/<int:pk>/accept/',RequestViewSet.as_view({'get':'accept_request'}),name='accept-request'),
+    path('requests/<int:pk>/accept',RequestViewSet.as_view({'get':'accept_request'}),name='accept-request'),
     # WORKS
-    path('requests/<int:pk>/decline/',RequestViewSet.as_view({'get':'decline_request'}),name='decline-request'),
+    path('requests/<int:pk>/decline',RequestViewSet.as_view({'get':'decline_request'}),name='decline-request'),
 
 ]
