ba315bf1a2ebce379e7ff35c9eaf103e76b64ccd
risqi2 <risqiikhsani12@gmail.com>
2023-05-17 08:39:39 +0700

    test

--- marketplace/schema.py
+++ marketplace/schema.py
@@ -4,7 +4,7 @@ from .models import Category, Ingredient
 import graphene
 from graphene_django import DjangoObjectType
 from .types import *
-
+from .models import *
 
 class Query(graphene.ObjectType):
     all_ingredients = graphene.List(IngredientType)
@@ -20,8 +20,34 @@ class Query(graphene.ObjectType):
         except Category.DoesNotExist:
             return None
         
+# class QuestionType(DjangoObjectType):
+#     class Meta:
+#         model = Question
+        
+# class QuestionMutation(graphene.Mutation):
+#     class Arguments:
+#         # The input arguments for this mutation
+#         text = graphene.String(required=True)
+#         id = graphene.ID()
+
+#     # The class attributes define the response of the mutation
+#     question = graphene.Field(QuestionType)
+
+#     @classmethod
+#     def mutate(cls, root, info, text, id):
+#         question = Question.objects.get(pk=id)
+#         question.text = text
+#         question.save()
+#         # Notice we return an instance of this mutation
+#         return QuestionMutation(question=question)
+
+
+# class Mutation(graphene.ObjectType):
+#     update_question = QuestionMutation.Field()
+
 class Mutation(graphene.ObjectType):
     pass
+
 
 schema = graphene.Schema(query=Query,mutation=Mutation)
 
---
+++ marketplace/types_v2.py
@@ -0,0 +1,29 @@
+# cookbook/ingredients/schema.py
+from graphene import relay
+import graphene
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+
+from .models import Category, Ingredient
+
+
+# Graphene will automatically map the Category model's fields onto the CategoryNode.
+# This is configured in the CategoryNode's Meta class (as you can see below)
+class CategoryNode(DjangoObjectType):
+    class Meta:
+        model = Category
+        filter_fields = ['name', 'ingredients']
+        interfaces = (relay.Node, )
+
+
+class IngredientNode(DjangoObjectType):
+    class Meta:
+        model = Ingredient
+        # Allow for some more advanced filtering here
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'notes': ['exact', 'icontains'],
+            'category': ['exact'],
+            'category__name': ['exact'],
+        }
+        interfaces = (relay.Node, )
